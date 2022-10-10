b3bbd817f21584d940f8a22c0e8a20c851f68157
AlexanderJCS <98898166+AlexanderJCS@users.noreply.github.com>
2022-10-10 16:54:01 -0400

    Add files via upload

---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+#include "parseData.h"
+#include "detect_exoplanets.h"
+
+using namespace std::chrono;
+
+
+int main()
+{
+
+	DetectExoplanets::FindPlanet planetFinder{ readData("data.csv"), 0.9999, 0.002, 1.5, 0.4 };
+	
+	auto start = high_resolution_clock::now();
+	auto planetFluxes = planetFinder.findPlanets();
+	auto stop = high_resolution_clock::now();
+
+	for (auto& flux : planetFluxes) {
+		std::cout << "Planet identified with flux: " << flux << "\n";
+	}
+
+	auto duration = duration_cast<milliseconds>(stop - start);
+	std::cout << "\nCompleted in " << duration.count() << " milliseconds\n";
+}
